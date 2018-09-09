13aa2ea634dc76a92130317ba6772f021ad9f022
vw9 <yuki.crot@gmail.com>
2018-09-09 23:29:36 +0900

    user登録フォームview更新 ルーティング更新 コントローラー更新

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'pages#index'
   get 'pages/help'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

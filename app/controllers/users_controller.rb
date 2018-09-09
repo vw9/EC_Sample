bc0aeb389fa178e659f613bc3a1e709e06408dd3
vw9 <yuki.crot@gmail.com>
2018-09-09 23:59:50 +0900

    createアクション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    binding.pry
+    @user = User.new(name: params[:user][:name], kana: params[:user][:kana], mail: params[:user][:mail], postal_code: params[:user][:postal_code], address: params[:user][:address])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end

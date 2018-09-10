9f659369dfb8fbf2473077761d0163e5b5b813f3
vw9 <yuki.crot@gmail.com>
2018-09-10 16:08:12 +0900

    ログアウト機能追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,18 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+
   private
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end

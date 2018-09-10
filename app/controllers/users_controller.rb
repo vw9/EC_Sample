8bdcb6a9486c9f0f644802fd0c42c8f2bf3667a5
vw9 <yuki.crot@gmail.com>
2018-09-10 10:17:41 +0900

    登録結果のフィードバック作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
     binding.pry
     @user = User.new(name: params[:user][:name], kana: params[:user][:kana], mail: params[:user][:mail], postal_code: params[:user][:postal_code], address: params[:user][:address])
     if @user.save
-      redirect_to root_path
+      redirect_to root_path, success: "登録が完了しました"
     else
+      flash.now[:danger] = "登録に失敗しました"
       render :new
     end
   end

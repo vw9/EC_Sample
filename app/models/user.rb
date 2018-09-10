f12b1213874ed8520a121bb6ed1afd3d0e92a2f0
vw9 <yuki.crot@gmail.com>
2018-09-10 10:03:47 +0900

    ヴァリデーション途中まで追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :name,
+    presence: true,
+    length: { maximum: 15, too_long: "15文字以上は入力できません" }
+  validates :kana,
+    presence: true,
+    length: { maximum: 25, too_long: "25文字以上は入力できません"}
+  validates :mail,
+    presence: true,
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :postal_code,
+    presence: true
 end

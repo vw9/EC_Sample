33bce36f39063e2928b8b46f3a60c5fa16fcdf70
vw9 <yuki.crot@gmail.com>
2018-10-05 16:28:17 +0900

    一覧画面　投稿画面　ルーティング　作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'pages/help'
 
   resources :users
+  resources :items
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'

234f619fa820c8f98f5badec3b6c5988eefb3dd5
vw9 <yuki.crot@gmail.com>
2018-10-06 13:09:28 +0900

    新規登録　def createアクション追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,9 +8,22 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  def enum
-    @item = Item.find(1)
-    @item.published!
+  def create
+    @item = Item.new(item_params)
+
+    respond_to do |format|
+      if @item.save
+        format.html { redirect_to new_item_path, notice: 'Item was Successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :price, :body, :brand, :category, :country)
   end
 
 end

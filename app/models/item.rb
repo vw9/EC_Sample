c61a16e85529daf8a3ef6fb110ecf7513818a8b7
vw9 <yuki.crot@gmail.com>
2018-10-08 13:59:07 +0900

    gem rmagick 追加 画像アップロードview訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,5 @@ class Item < ApplicationRecord
   enum brand: { A:0, B:1, C:2, D:3, E:4 }
   enum category: { OUTER:0, TOPS:1, BOTOMS:2, SHOES:3 }
   enum country: { JPN:0, USA:1, GTR:2, DEU:3, FRA:4, ITA:5 }
+  mount_uploader :image, ImageUploader
 end

8aecd9bb4969465a2975527c16cd50cd258b3aeb
vw9 <yuki.crot@gmail.com>
2018-10-09 11:09:55 +0900

    カラー投稿フォーム　保存　詳細　編集　削除機能追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.all
+    @items = Item.all.includes(:colors)
   end
 
   def new
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
 
     respond_to do |format|
-      if @item.save
+      if @item.save && @item.colors.create(color: params[:color])
         format.html { redirect_to new_item_path, notice: 'Item was Successfully created.' }
       else
         format.html { render :new }
@@ -30,7 +30,7 @@ class ItemsController < ApplicationController
 
   def update
     respond_to do |format|
-      if @item.update(item_params)
+      if @item.update(item_params) && @item.colors.update(color: params[:color])
         format.html { redirect_to @item, notice: 'Item was successfully updated.' }
       else
         format.html { render :edit }
@@ -45,6 +45,8 @@ class ItemsController < ApplicationController
       format.html { redirect_to items_path, notice: 'Item was successfully destroyed.' }
     end
   end
+
+
 
   private
 
